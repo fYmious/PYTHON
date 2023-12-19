deda7fe1fff91c143830f2289cc77f178e0e82f2
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2023-12-19 16:04:12 +0500

    msg

--- 003ege/main.py
+++ 003ege/main.py
@@ -1,8 +1,2 @@
-print("x y z")
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            f = ((not x) and y and z) or ((not x) and (not y))
-            if f == 1:
-                print(x, y, z)
-
+for x, y, w, z in 0, 1:
+    print(x, y, w, z)
