b6e694aa2a7411d9750d099e36c553d380332c00
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2023-12-20 06:55:11 +0500

    jinja

--- 004flask/app.py
+++ 004flask/app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", )
+
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
