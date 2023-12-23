1bf881ea08c2cd1f004ceb4338a4c31f5bfe49e1
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2023-12-23 20:18:54 +0500

    m

--- 003ege/main.py
+++ 003ege/main.py
@@ -1,7 +1,16 @@
-a: dict = {1: "salad",
-           2: "nigger",
-           3: "ovosh"}
+from itertools import product, permutations
 
-print(**a)
 
-print("123piska")
+def f(x, y, z, w):
+    return not (z <= w) or (x <= y) or (not z)
+
+
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    table = [(0, a1, a2, 0),
+             (1, 0, a3, a4),
+             (a5, 1, a6, a7)]
+
+    if len(set(table)) == len(table):
+        for permutation in permutations("xyzw"):
+            if [f(**dict(zip(permutation, row))) for row in table] == [0, 0, 0]:
+                print(permutation)
