dcc3aacf2b3ad321ae567e98c427e83498d73129
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2023-12-23 18:14:08 +0500

    gt

--- 003ege/2.py
+++ 003ege/2.py
@@ -15,3 +15,5 @@ for a1, a2, a3, a4, a5, a6, a7, a8 in product([0, 1], repeat=8):
         for p in permutations("xywzu"):
             if [f(**dict(zip(p, r))) for r in t] == [0, 0, 0, 0]:
                 print(p)
+
+print()
