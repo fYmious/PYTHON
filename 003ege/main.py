4b31edc5c36b1b3aefb42f0c2ebac1719b975bd0
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2023-12-24 11:41:43 +0500

    бебра

--- 003ege/main.py
+++ 003ege/main.py
@@ -1,16 +1,1 @@
-from itertools import product, permutations
-
-
-def f(x, y, z, w):
-    return (not (z <= w)) or (x <= y) or (not x)
-
-
-for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
-    table = [(0, a1, a2, 0),
-             (1, 0, a3, a4),
-             (a5, 1, a6, a7)]
-
-    if len(set(table)) == len(table):
-        for permutation in permutations("xyzw"):
-            if [f(**dict(zip(permutation, row))) for row in table] == [0, 0, 0]:
-                print(permutation)
+print("нюхай бебрятинку клоун")
