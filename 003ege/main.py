bebf7d1b9cfc0f2d60e882f50f27ee219d6ec6e1
nikita <doschennicovn@gmail.com>
2024-01-01 17:07:02 +0500

    msg

--- 003ege/main.py
+++ 003ege/main.py
@@ -1,18 +1,47 @@
-G: float = 6.67 * 10 ** -11
-EARTH_MASS: float = 5.98 * 10 ** 24
-EARTH_RADIOUS: float = 6.37 * 10 ** 6
-SUN_MASS: float = 1.98 * 10 ** 30
-SUN_RADIOUS: float = 6.95 * 10 ** 8
-AE: float = 1.49 * 10 ** 11
+def to_any(num: int, base: int = 3) -> list:
+    translate: dict = {1: "1",
+                       2: "2",
+                       3: "3",
+                       4: "4",
+                       5: "5",
+                       6: "6",
+                       7: "7",
+                       8: "8",
+                       9: "9",
+                       10: "A",
+                       11: "B",
+                       12: "C",
+                       13: "D",
+                       14: "E",
+                       15: "F"}
+    res: list[int] = []
+    while num > 0:
+        res.append(translate[num % base])
+        num //= base
 
-planet_mass: float = float(input("enter the planet mass (in Earth's masses) : ")) * EARTH_MASS
-planet_radious: float = float(input("enter the planet radious (in Earth's radiuoses): ")) * EARTH_RADIOUS
-distance_from_sun: float = float(input("enter the distance to the sun (in a.e.): ")) * AE
+    return res[::-1]
 
-r = distance_from_sun + SUN_RADIOUS + planet_radious
 
-f: float = (G * planet_mass * EARTH_MASS) / (r ** 2)
-g: float = (G * planet_mass) / (planet_radious ** 2)
+def task(n: int) -> int:
+    third: list[str] = to_any(n)
+    if len(third) % 2 != 0:
+        third = ["1"] + third
 
-print(f)
-print(g)
+    if sum(int(i) for i in third):
+        third = third + third[:2]
+    else:
+        remainder: list[str] = to_any(n % 5)
+        third = third + remainder
+
+    third = list(str(int("".join(third))))
+
+    if third[0] == "2":
+        third = third[1:]
+
+    if third[-1] == third[-2]:
+        third = third[:-1]
+
+    return int("".join(third), 3)
+
+
+print(task(14))
