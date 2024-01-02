d4b753857165ab431d8e27d6638b8e148d15c8d7
nikita <doschennicovn@gmail.com>
2024-01-02 11:29:15 +0500

    nsg

--- 003ege/main.py
+++ 003ege/main.py
@@ -1,11 +1,13 @@
 from itertools import permutations, product
 
+
 def check(num: tuple[int]) -> bool:
     for i in range(len(num) - 1):
         if (num[i] + num[i + 1]) % 2 == 0:
             return False
 
     return True
+
 
 counter: int = 0
 for i in product([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], repeat=3):
