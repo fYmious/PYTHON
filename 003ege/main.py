d2ef9cb328f030e67a3300ea5c6f6ce4b2fc5e06
nikita <doschennicovn@gmail.com>
2024-01-02 11:37:11 +0500

    nsgh

--- 003ege/main.py
+++ 003ege/main.py
@@ -10,9 +10,12 @@ def check(num: tuple[int]) -> bool:
 
 
 counter: int = 0
-for i in product([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], repeat=3):
-    if len(set(i)) == len(i):
-        if check(i):
-            counter += 1
+for i in product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], repeat=3):
+    if i[0] != 0:
+        if len(set(i)) == len(i):
+            if check(i):
+                print(i)
+                counter += 1
+
 
 print(counter)
