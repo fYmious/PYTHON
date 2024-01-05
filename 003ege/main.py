be815a21290b878bee72714f0c86f62d6c5c4a2b
dolbik_2 <132186293+fYmious@users.noreply.github.com>
2024-01-05 09:27:41 +0500

    global chenges

--- 003ege/main.py
+++ 003ege/main.py
@@ -1,11 +1,10 @@
-from itertools import permutations
+def f(n, a, b):
+    x = 9 + n * (a + 11) - 58
+    y = -10 + n * (b + 9) - 18
+    return x == 0 and y == 0
 
-counter: int = 0
-for i in set(permutations("ВОДОПАД")):
-    word = ''.join(i)
-    for l in "ОА": word = word.replace(l, '1')
-    if "11" not in word:
-        counter += 1
 
-print(counter)
-
+for n in range(1000):
+    if any([f(n, a, b) for a in range(-150, 150) for b in range(-150, 150)]):
+        print(n)
+        break
